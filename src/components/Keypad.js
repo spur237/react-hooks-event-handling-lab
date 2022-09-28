c36a1a3b4f57e6bb2a992142dae0d9c633c17fb6
= <=>
2022-09-28 18:13:12 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
+// Code Keypad Component Here
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange= {() => console.log("Entering password...")
+        }/>
+            
+        </div>
     )
 }
 
